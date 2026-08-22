f5b6103af271c91fe4b1a294d71f74426d6e4f36
Ld2000king <liavdimri12@gmail.com>
2026-08-22 19:25:04 +0300

    Bump PWA cache for shop-code release

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="barber-app-v4",ROOT=new URL("./",self.registration.scope).pathname,SHELL=[ROOT,`${ROOT}manifest.webmanifest`,`${ROOT}barbers-logo.png`];
-self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL))));
-self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+const CACHE="barber-app-v5",ROOT=new URL("./",self.registration.scope).pathname,SHELL=[ROOT,`${ROOT}manifest.webmanifest`,`${ROOT}barbers-logo.png`];
+self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(response=>response||caches.match(ROOT))))});
